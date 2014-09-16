cced5d404e1c2bf983e631948919b4cc1078d420
Alex Cantu <miguel.cantu@rackspace.com>
2014-09-16 15:15:11 -0500

    Update generateInterface.sh

--- RPC9.0/generateInterface.sh
+++ RPC9.0/generateInterface.sh
@@ -3,35 +3,43 @@
 #Change the IP address to match yours
 
 for i in {161..174}; do
-cat << EOF > /tmp/host-network.$i.cfg
+
+cat << EOF > /tmp/global.$i.cfg
 auto eth0
-iface eth0 inet manual
+iface em1 inet manual
 
-auto eth0.101
-iface eth0.101 inet static
-	address 10.0.0.$i
-	netmask 255.255.255.0
+
+iface eth0.111 inet manual 
 	vlan-raw-device eth0
-EOF
-
-#Management bridge
-cat << EOF > /tmp/mgmt.$i.cfg
-
-auto eth0.102
-iface eth0.102 inet manual 
+	
+iface eth0.222 inet manual
 	vlan-raw-device eth0
-
+	
 auto br-mgmt
-auto br-mgmt inet static
+iface br-mgmt inet static
 	bridge_stp off
 	bridge_waitport 0
 	bridge_fd 0
-	# Bridge port references tagged interface
-	bridge_ports eth0.102
-	address 10.1.0.$i
-	netmask 255.255.255.0
-	gateway 10.1.0.1
+	bridge_ports eth0
+	address 10.241.1.$i
+	netmask 255.255.0.0
+	gateway 10.241.253.253
 	dns-nameservers 8.8.8.8 4.2.2.2
+	
+auto br-vlan
+iface br-vlan inet manual
+	bridge_stp off
+	bridge_waitport 0
+	bridge_fd 0
+	bridge_ports eth0.111
+	
+auto br-vxlan
+iface br-vxlan inet manual
+	bridge_stp off
+	bridge_waitport 0
+	bridge_fd 0
+	bridge_ports eth0.222
+
 EOF
 
 #scp /tmp/host-network.$i.cfg root@10.0.0.$i:/etc/network/interfaces.d/
